f47aa8f77dcf42d49e6c5dab07fd2c5443fd5f5e
vboxuser <vboxuser@akankshadhyani.myguest.virtualbox.org>
2024-09-05 18:05:33 +0530

    cosmos.msg.v1 signer option annotation added

--- x/cctp/types/query.pb.go
+++ x/cctp/types/query.pb.go
@@ -2716,6 +2716,7 @@ func _Query_LocalDomain_Handler(srv interface{}, ctx context.Context, dec func(i
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "circle.cctp.v1.Query",
 	HandlerType: (*QueryServer)(nil),
